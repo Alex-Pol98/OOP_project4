7d53654d63dea3788b056ff7c7343b83adc6bd63
Alex-Pol98 <quaker1925@gmail.com>
2018-02-18 14:48:29 +0200

    Delete nonAccessibleSquare.cpp

--- OOP_project/Files/nonAccessibleSquare.cpp
+++
@@ -1,7 +0,0 @@
-#include "nonAccessibleSquare.h"
-//#include "Square.h"
-
-nonAccessibleSquare::nonAccessibleSquare(int givenX, int givenY):Square(SQUARE_TYPE_NON_ACCESSIBLE, givenX, givenY)
-{}
-
-nonAccessibleSquare::~nonAccessibleSquare(){}
